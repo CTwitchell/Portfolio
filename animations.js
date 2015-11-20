c6bea94cc45437ffe467f528c28796967a3357ab
CTwitchell <YellingAtFish@gmail.com>
2015-11-20 10:23:36 -0700

    added jquery.

--- animations.js
+++ animations.js
@@ -1,3 +1,16 @@
 $(document).ready(function () {
-    
+
+    $(".tile11").hover(function () {
+        $(".tile9").css("width", "15%");
+         $(".tile10").css("width", "15%");
+         $(".tile12").css("width", "15%");
+         $(".tile11").css("width", "55%");
+        $(".resume").css("font-size", "75px");
+    }, function () {
+        $(".tile9").css("width", "25%");
+        $(".tile10").css("width", "25%");
+         $(".tile12").css("width", "25%");
+         $(".tile11").css("width", "25%");
+        $(".resume").css("font-size", "55px");
+    });
 });
