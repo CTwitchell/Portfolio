ebec6eb2bad9247a21b9a72a03b7f5c18ee4c170
CTwitchell <YellingAtFish@gmail.com>
2015-12-01 10:28:21 -0700

    added Resume and project tiles

--- animations.js
+++ animations.js
@@ -13,4 +13,29 @@ $(document).ready(function () {
          $(".tile11").css("width", "25%");
         $(".resume").css("font-size", "55px");
     });
+    $(".tile11").click(function () {
+        $(".resumedisplaybox").css("display", "inline-block")
+    });
+    $(".close").click(function () {
+        $(".resumedisplaybox").css("display", "none")
+    });
+    $(".tile14").hover(function () {
+        $(".tile13").css("width", "15%");
+         $(".tile14").css("width", "55%");
+         $(".tile15").css("width", "15%");
+         $(".tile16").css("width", "15%");
+        $(".projects").css("font-size", "75px");
+    }, function () {
+        $(".tile13").css("width", "25%");
+        $(".tile14").css("width", "25%");
+         $(".tile15").css("width", "25%");
+         $(".tile16").css("width", "25%");
+        $(".projects").css("font-size", "55px");
+    });
+     $(".tile14").click(function () {
+        $(".projectdisplaybox").css("display", "inline-block")
+    });
+    $(".close2").click(function () {
+        $(".projectdisplaybox").css("display", "none")
+    });
 });
